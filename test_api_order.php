4044e871dbdfe55ec9a53f8811846834f4dc4b04
carlkelvin8 <carlkelvin8@users.noreply.github.com>
2026-05-19 20:54:48 +0800

    chore: remove temporary test scripts

--- test_api_order.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->bootstrap();
-
-$order = App\Models\Order::with(['driverProfile.user', 'customer', 'orderItems', 'service', 'partner'])->latest()->first();
-echo json_encode($order ? $order->toArray() : ['error' => 'No order found'], JSON_PRETTY_PRINT);
